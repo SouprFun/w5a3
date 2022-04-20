5c4a35dec74c61939ae3657cc9eba60631b542f2
Brian Campbell <bry123ftw@gmail.com>
2022-04-19 22:20:27 -0500

    started function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+let Collection = [];
+
+function addToCollection(Title, Artist, YearPub){
+    let album = {
+        title: Title,
+        artist: Artist,
+        publish: YearPub
+    }
+}
