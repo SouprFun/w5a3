a834b957a95078993f6f160942d2d8af95637697
Brian Campbell <bry123ftw@gmail.com>
2022-04-19 22:34:21 -0500

    added 6 albums as objects to array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,5 +6,16 @@ function addToCollection(Title, Artist, YearPub){
         title: Title,
         artist: Artist,
         publish: YearPub
-    }
+    };
+    Collection.push(album) 
+    return album
 }
+
+addToCollection( 'Eternal Blue', 'Spiritbox', '2021' );
+addToCollection( 'Verkligheten', 'Soilwork', '2019' );
+addToCollection( 'War/Peace', 'Demon Hunter', '2019' );
+addToCollection( 'Underworld', 'Symphony X', '2015' );
+addToCollection( 'Helix', 'Amaranthe', '2018' );
+addToCollection( 'Abyss', 'Unlease The Archers', '2020' );
+
+console.log(Collection);
